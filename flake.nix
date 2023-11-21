ab7c31c9c6ee4d4f433f34f33211569656e4f60c
Kristoffer Søholm <k.soeholm@gmail.com>
2023-11-21 16:53:03 +0100

    Add flake setup

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Valve GameNetworkingSockets Wrapper";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+      in with pkgs;
+      let
+        rust = rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+        };
+        protobuf = protobuf3_20;
+      in {
+        devShell = mkShell {
+          buildInputs = [ rust clang protobuf openssl pkg-config  ];
+          PROTOC = "${protobuf}/bin/protoc";
+          LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
+          LD_LIBRARY_PATH =
+            lib.makeLibraryPath [ clangStdenv.cc.cc.lib openssl protobuf ];
+          CPLUS_INCLUDE_PATH = "${openssl.dev}/include";
+        };
+      });
+}
